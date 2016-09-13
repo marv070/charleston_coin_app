dff5fe0eaf6d17f9a02b8e551a59213411107f28
Adam Dittebrand <Adam Dittebrand>
2016-09-13 16:54:25 -0400

    defined hashes for coins and got dimes and quarter tests to run

--- Coin_Changer.rb
+++ Coin_Changer.rb
@@ -1,14 +1,14 @@
 def change(cents_recieved)
 	coins = {}
-	if cents_recieved >= 1
-		coins[:penny] = cents_recieved
-	elsif cents_recieved >= 5
-		coins[:nickel] = cents_recieved
-	elsif cents_recieved >= 10
-		coins[:dime] = cents_recieved
-	elsif cents_recieved >= 25
-		coins[:quarter] = cents_recieved
+	if 
+		cents_recieved >= 25
+		coins[:quarter] = cents_recieved / 25
+	elsif 
+		cents_recieved >= 10
+		coins[:dime] = cents_recieved / 10
 	end
+	
+		
 	coins
 
 
--- Coin_Changer_test.rb
+++ Coin_Changer_test.rb
@@ -8,16 +8,30 @@ class TestCoinChanger < Minitest::Test
 		assert_equal({}, change(cents_recieved))
 	end
 
-	def test_1_cent_returns_penny
-		cents_recieved = 1
-		assert_equal({:penny => 1}, change(cents_recieved))
+	# def test_1_cent_returns_penny
+	# 	cents_recieved = 1
+	# 	assert_equal({:penny => 1}, change(cents_recieved))
+	# end
+
+	# def test_2_cents_returns_2_pennies
+	# 	cents_recieved = 2
+	# 	assert_equal({:penny => 2}, change(cents_recieved))
+		
+	# end
+
+	def test_25_cents_returns_quarter
+		cents_recieved = 25
+		assert_equal({:quarter => 1}, change(cents_recieved))
+	end
+	def test_75_cents_returns_3_quarters
+		cents_recieved = 75
+		assert_equal({:quarter => 3}, change(cents_recieved))
+	end
+	def test_20_cents_returns_2_dimes
+		cents_recieved = 20
+		assert_equal({:dime => 2}, change(cents_recieved))
 	end
 
-	def test_2_cents_returns_2_pennies
-		cents_recieved = 2
-		assert_equal({:penny => 2}, change(cents_recieved))
-		
-	end
 
 end
 
