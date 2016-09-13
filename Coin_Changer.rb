889ee0d2189e136601821dbaceb7aded7d3ccc9c
Adam Dittebrand <Adam Dittebrand>
2016-09-13 16:36:09 -0400

    test for 2 cents

--- Coin_Changer.rb
+++ Coin_Changer.rb
@@ -1,3 +1,18 @@
-def change(cents)
-	{}
+def change(cents_recieved)
+	coins = {}
+	if cents_recieved >= 1
+		coins[:penny] = cents_recieved
+	elsif cents_recieved >= 5
+		coins[:nickel] = cents_recieved
+	elsif cents_recieved >= 10
+		coins[:dime] = cents_recieved
+	elsif cents_recieved >= 25
+		coins[:quarter] = cents_recieved
+	end
+	coins
+
+
+
+
+	
 end
--- Coin_Changer_test.rb
+++ Coin_Changer_test.rb
@@ -4,7 +4,20 @@ require_relative "Coin_Changer.rb"
 class TestCoinChanger < Minitest::Test
 
 	def test_0_cents_returns_empty_hash
-		assert_equal({}, change(0))
+		cents_recieved = 0
+		assert_equal({}, change(cents_recieved))
 	end
+
+	def test_1_cent_returns_penny
+		cents_recieved = 1
+		assert_equal({:penny => 1}, change(cents_recieved))
+	end
+
+	def test_2_cents_returns_2_pennies
+		cents_recieved = 2
+		assert_equal({:penny => 2}, change(cents_recieved))
+		
+	end
+
 end
 
