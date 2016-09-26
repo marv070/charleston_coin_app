d6f37f7b16c777d72f5b07182e97ee7bb95df8c6
Adam Dittebrand <Adam Dittebrand>
2016-09-26 12:11:13 -0400

    forgot about the views file for sinatra. added it, program is working. Time to add some fun stuff.

--- coinapp.rb
+++ coinapp.rb
@@ -22,9 +22,9 @@ post '/pin' do
 	redirect '/fav_numbers?name=' + name +"&age=" + age
 end
 
-get '/fav_numbers' do
-	name = params[:name]
-	age = params[:age]
-	erb :favorite_numbers, :locals => {:name => name, :age => age}
-end
+# get '/fav_numbers' do
+# 	name = params[:name]
+# 	age = params[:age]
+# 	erb :favorite_numbers, :locals => {:name => name, :age => age}
+# end
 
