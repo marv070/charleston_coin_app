55d5d97854bc2f98c5de2bdb414ef7a579dda447
Marvin Laucher <marv070@gmail.com>
2016-10-06 13:35:38 -0400

    add change to validate chars in name input box. return message to screen

--- public/js/form_validation.js
+++ public/js/form_validation.js
@@ -1,7 +1,7 @@
 function validateForm(form,box) {
   var value = document.forms[form][box].value;
    if (value.length < 3) {
-       document.getElementById("error").innerHTML = "please input your name!";
+       document.getElementById("error").innerHTML = "Try Again but this time enter your name!";
        return false;
    }
 }
